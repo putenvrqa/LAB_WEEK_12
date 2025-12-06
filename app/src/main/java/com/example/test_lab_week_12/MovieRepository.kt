c32c441fe681f970b64c368487aebdf9ff4673ba
putee <puyi.novariqa@gmail.com>
2025-12-07 02:54:58 +0700

    Commit No. 2 – Upgrading app with flow

---
+++ app/src/main/java/com/example/test_lab_week_12/MovieRepository.kt
@@ -0,0 +1,22 @@
+package com.example.test_lab_week_12
+
+import com.example.test_lab_week_12.api.MovieService
+import com.example.test_lab_week_12.model.Movie
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+
+class MovieRepository(private val movieService: MovieService) {
+
+    private val apiKey = "9e4b78023ccf118352e70d93fafc84d0"
+
+    // fetch movies from the API
+    // this function returns a Flow of Movie objects
+    fun fetchMovies(): Flow<List<Movie>> {
+        return flow {
+            // emit the list of popular movies from the API
+            emit(movieService.getPopularMovies(apiKey).results)
+        }.flowOn(Dispatchers.IO)  // run on background thread
+    }
+}
---
+++ app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.test_lab_week_12
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.test_lab_week_12.model.Movie
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
+
+class MovieViewModel(private val movieRepository: MovieRepository) : ViewModel() {
+
+    private val _popularMovies = MutableStateFlow(emptyList<Movie>())
+    val popularMovies: StateFlow<List<Movie>> = _popularMovies
+
+    private val _error = MutableStateFlow("")
+    val error: StateFlow<String> = _error
+
+    init {
+        fetchPopularMovies()
+    }
+
+    private fun fetchPopularMovies() {
+        viewModelScope.launch(Dispatchers.IO) {
+
+            movieRepository.fetchMovies()
+                .catch { e ->
+                    // error dari Flow
+                    _error.value = "An exception occurred: ${e.message}"
+                }
+                .collect { movieList ->
+                    _popularMovies.value = movieList
+                }
+        }
+    }
+}
